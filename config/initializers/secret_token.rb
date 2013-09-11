1016bae7872cea6cc1f6aaf0a16752d31b4e8ec1
Jessica Lin <jessica.lin8@gmail.com>
2013-09-11 16:13:39 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Awesometrain::Application.config.secret_key_base = 'ff0b1f097129cbdbeb0ca64d3105ae049248e525dc502b77f1fa3a3eec0f4aa3616e28adee403337662f0d645e9cc281aa508a26dd1ce77e1c94c31a7afa5262'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Awesometrain::Application.config.session_store :cookie_store, key: '_awesometrain_session'
